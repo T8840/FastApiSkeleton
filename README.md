e086bb052224cb9a57353265fda946de7787c05f
ATao <xitao_wang@sui.com>
2020-08-27 10:33:07 +0800

    update config

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ uvicorn main:app --reload
 3.提交model变更命令：  
   alembic revision --autogenerate -m '提交说明'  
   alembic upgrade head
+  
+### 部署命令
+- docker build -t fastapi .
+- docker run -d --name mycontainer -v $(pwd):/app -p  $(port):80 fastapi
+  - Example：  
+    docker run -d --name fastapi-platform -v /home/FastApiSkeleton/app:/app -p  8000:80 fastapi
--- app/core/config.py
+++ app/core/config.py
@@ -1,37 +1,37 @@
-import logging
-import sys
-from typing import List
-
-from databases import DatabaseURL
-# # from loguru import logger
-from starlette.config import Config
-from starlette.datastructures import CommaSeparatedStrings, Secret
-
-# from app.core.logging import InterceptHandler
+# import logging
+# import sys
+# from typing import List
 #
-API_PREFIX = "/api"
+# from databases import DatabaseURL
+# # from loguru import logger
+# from starlette.config import Config
+# from starlette.datastructures import CommaSeparatedStrings, Secret
+#
+# # from app.core.logging import InterceptHandler
+#
+# API_PREFIX = "/api"
 #
 # JWT_TOKEN_PREFIX = "Token"  # noqa: S105
 # VERSION = "0.0.0"
 #
-config = Config(".env")
+# config = Config(".env")
 #
-DEBUG: bool = config("DEBUG", cast=bool, default=False)
-
+# DEBUG: bool = config("DEBUG", cast=bool, default=False)
+#
 # DATABASE_URL: DatabaseURL = config("DB_CONNECTION", cast=DatabaseURL)
-MAX_CONNECTIONS_COUNT: int = config("MAX_CONNECTIONS_COUNT", cast=int, default=10)
-MIN_CONNECTIONS_COUNT: int = config("MIN_CONNECTIONS_COUNT", cast=int, default=10)
-
-SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
-
-PROJECT_NAME: str = config("PROJECT_NAME", default="FastAPI example application")
-ALLOWED_HOSTS: List[str] = config(
-    "ALLOWED_HOSTS", cast=CommaSeparatedStrings, default=""
-)
-
-# logging configuration
-
-LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
+# MAX_CONNECTIONS_COUNT: int = config("MAX_CONNECTIONS_COUNT", cast=int, default=10)
+# MIN_CONNECTIONS_COUNT: int = config("MIN_CONNECTIONS_COUNT", cast=int, default=10)
+#
+# SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
+#
+# PROJECT_NAME: str = config("PROJECT_NAME", default="FastAPI example application")
+# ALLOWED_HOSTS: List[str] = config(
+#     "ALLOWED_HOSTS", cast=CommaSeparatedStrings, default=""
+# )
+#
+# # logging configuration
+#
+# LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
 # logging.basicConfig(
 #     handlers=[InterceptHandler(level=LOGGING_LEVEL)], level=LOGGING_LEVEL
 # )
@@ -63,9 +63,9 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 import os
 from datetime import timedelta
-# SECRET_KEY = b"SECRET_KEY"
-# if not SECRET_KEY:
-#     SECRET_KEY = os.urandom(32)
+SECRET_KEY = b"SECRET_KEY"
+if not SECRET_KEY:
+    SECRET_KEY = os.urandom(32)
 
 # Token 60 minutes * 24 hours * 8 days = 8 days
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8
@@ -84,3 +84,5 @@ EMAIL_RESET_TOKEN_EXPIRE_HOURS = 48
 EMAIL_TEMPLATES_DIR = "/app/app/email-templates/build"
 EMAILS_ENABLED = SMTP_HOST and SMTP_PORT and EMAILS_FROM_EMAIL
 
+PROJECT_NAME = "MyProject"
+SERVER_HOST = 'http://127.0.0.1:8000'
